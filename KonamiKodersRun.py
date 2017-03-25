62c8f8a0ac5b6fab084bedfcba643f761752577a
Spiderfav <ruifavinha2002@gmail.com>
2017-03-25 18:39:05 +0000

    All

--- KonamiKodersRun.py
+++ KonamiKodersRun.py
@@ -21,22 +21,22 @@ finch666 = KonamiKoders.OurFinch() # finch666 is equal to the class name
 # My hand is 23.33 when room temp == 22.08
 # Aprox. bottle 25.42
 
+while True:
+    left_light, right_light = finch.light()
+    print ('Lights %5.3f, %5.3f' % (left_light, right_light))
+    if  float(left_light - right_light) <= 0.08 and float(left_light - right_light) >= 0:
+        finch.wheels(0.25,0.25)
 
-left_light, right_light = finch.light()
-print ('Lights %5.3f, %5.3f' % (left_light, right_light))
-if  float(left_light - right_light) <= 0.08 and float(left_light - right_light) >= 0:
-    finch.wheels(0.25,0.25)
+        # If the finch is getting more light in the right than left
+    elif right_light > left_light:
+        print("Going right")
+        finch.wheels(0.25,0)
+        sleep(1.0)
 
-# If the finch is getting more light in the right than left
-elif right_light > left_light:
-    print("Going right")
-    finch.wheels(0.25,0)
-    sleep(1.0)
-
-# If the finch is getting more light in the left than right        
-elif left_light > right_light:
-    print("Going left")
-    finch.wheels(0,0.25)
-    sleep(1.0)
+        # If the finch is getting more light in the left than right        
+    elif left_light > right_light:
+        print("Going left")
+        finch.wheels(0,0.25)
+        sleep(1.0)
     
 
